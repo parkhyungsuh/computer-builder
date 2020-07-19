77c56f691996ee5d587f6197a4f0248312f150ed
parkhs2018 <parkhs2018@gmail.com>
2020-07-20 03:32:08 +0900

    Filter chip add and remove test

--- src/Components/ItemFilter.js
+++ src/Components/ItemFilter.js
@@ -9,9 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-const handleDelete = () => {
-    console.log('chipchip')
-};
+
 const useStyle = makeStyles((theme) => ({
     root:{
 
@@ -20,16 +18,34 @@ const useStyle = makeStyles((theme) => ({
         flexBasis:150,
         lineHeight:2,
     },
-    chips:{
-    },
     card: {
     }
 }));
 
 const ItemFilter = () => {
     const classes = useStyle();
-
+    const [count, setCount] = useState(0);
     const [expanded, setExpanded] = useState(false);
+    const [filterList, setFilterList] = useState([
+    ]);
+    const core =[
+        { label: '4core'},
+        { label: '6core'},
+        { label: '8core'},
+        { label: '12core'}
+    ];
+    const handleDelete = (chipToDelete) => () => {
+        setFilterList(
+                (chips) => chips.filter((chip => chip.key !== chipToDelete.key))
+            );
+    };
+    const handleAdd = (a,b) => () => {
+        console.log(a,b);
+        let filterTemp = filterList.concat({key:a, label:b});
+        setFilterList(filterTemp);
+        setCount(count + 1);
+        
+    }
     const handleChange = (filter) => (event, isExpanded) => {
         setExpanded(isExpanded ? filter : false);
       };
@@ -39,12 +55,16 @@ const ItemFilter = () => {
             <Accordion expanded={expanded === 'filter'} onChange={handleChange('filter')}>
                 <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="filter" id="filter">
                     <Typography className={classes.heading}>Filter</Typography>
-                    <div className={classes.chips}>
-                        <Chip label="6core" onDelete={handleDelete} />
-                        <Chip label="6core" onDelete={handleDelete} />
-                        <Chip label="6core" onDelete={handleDelete} />
-                        <Chip label="6core" onDelete={handleDelete} />
-                        <Chip label="6core" onDelete={handleDelete} />
+                    <div>
+                        {filterList.map((data) => {
+                            return(
+                                <Chip
+                                key={data.key}
+                                label={data.label}
+                                onDelete={handleDelete(data)}
+                                />
+                            )
+                        })}
                     </div>
                 </AccordionSummary>
                 <AccordionDetails>
@@ -52,12 +72,15 @@ const ItemFilter = () => {
                         <Grid item sm={3} xs={6}>
                             <Card>
                                 <Typography>코어수</Typography>
-                                <Chip label="6core"/>
-                                <Chip label="6core"/>
-                                <Chip label="6core"/>
-                                <Chip label="6core"/>
-                                <Chip label="6core"/>
-                                <Chip label="6core"/>
+                                {core.map((data) => {
+                                    return (
+                                        <Chip
+                                            key={data.label}
+                                            label={data.label}
+                                            onClick={handleAdd(count,data.label)}
+                                        />
+                                    )
+                                })}
                             </Card>
                         </Grid>
                         <Grid item sm={3} xs={6}>
